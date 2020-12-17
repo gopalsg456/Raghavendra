9e3ace42554cb9eb6ce0c8cc89e6cf8594c30084
gopal <gopalsg456@gmail.com>
2020-12-18 04:16:40 +0530

    initial commit

---
+++ raghavendra.java
@@ -0,0 +1,14 @@
+public class raghavendra{
+
+       public static void main(String[] args){
+	   
+	        for (int i=1;i<=10;i++){
+			
+			     System.out.println("Sri Guru Raghavandraya Namaha... "+i);
+				 
+				 }
+				 
+				 
+				 }
+				 
+	}
